96fcccb5b3965080873e74a96828706038fff328
Aubry <p.aubry@lectra.com>
2018-12-10 17:08:03 +0100

    This is my first commit

---
+++ LectraGitkrakenTuto/src/Animal.java
@@ -0,0 +1,38 @@
+
+public class Animal {
+	private String name;
+	private int age;
+	private double poids;
+	
+	public Animal(String name, int age, double poids) {
+		this.name = name;
+		this.age = age;
+		this.poids = poids;
+	}
+
+	private String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
+	}
+
+	private int getAge() {
+		return age;
+	}
+
+	private void setAge(int age) {
+		this.age = age;
+	}
+
+	private double getPoids() {
+		return poids;
+	}
+
+	private void setPoids(double poids) {
+		this.poids = poids;
+	}
+
+	
+}
